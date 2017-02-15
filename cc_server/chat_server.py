3ba5c24edca3d18c7404b93a3156ee2c79f21196
TyroneL <tyronelagore@gmail.com>
2017-02-15 11:49:14 -0700

    added a testcompile script to ensure syntax

--- cc_server/chat_server.py
+++ cc_server/chat_server.py
@@ -4,14 +4,20 @@ class ChatServer:
     """Chat server class to handle chat server interactions"""
     # continue
     def __init__(self):
+        """ChatServer Constructor"""
         self._chat_groups = {}
+        self._connected_clients = {}
 
     def listen():
         """Listen for a client"""
 
-
     def worker():
-
+        """Handle a client"""
 
 class Client:
     """Representation of a client"""
+    def __init__(self):
+        """Client Constructor"""
+
+class ChatGroup:
+    """Representation of a chat group"""
