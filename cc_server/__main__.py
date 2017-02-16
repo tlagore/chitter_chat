24a31594c2173ad820523e18cbe8773c8a7c07c3
TyroneL <tyronelagore@gmail.com>
2017-02-16 14:59:17 -0700

    added some socket logic in ChatServer, __main__.py has been implemented to parse port and create ChatServer

--- cc_server/__main__.py
+++ cc_server/__main__.py
@@ -1,12 +1,28 @@
 import sys
+import numbers
+from chat_server import ChatServer
 
 def main(args=None):
     """Entry point for chat_server"""
-    if args is None:
-        args = sys.argv[1:]
-
-    # entry code here
-
-
+    
+    if len(sys.argv) != 2:
+        print("Invalid usage. Usage:")
+        print("%s [port_number]" % sys.argv[0])
+    else:
+        try:
+            port = int(sys.argv[1]);
+            
+            if port > 1024 and port <= 65535:
+                server = ChatServer(port)
+                server.start_server()
+            else:
+                print("Please specify a port between 1025 and 65535")
+                print("Ports 1-1024 are used by common applications...")
+                
+        except ValueError:
+            print("%s is not a valid port number." % sys.argv[1])
+        finally:
+            print("exitting...")
+            
 if __name__ == "__main__":
     main()
--- cc_server/chat_server.py
+++ cc_server/chat_server.py
@@ -1,18 +1,51 @@
 import threading
+import socket
+import re
 
 class ChatServer:
     """Chat server class to handle chat server interactions"""
     # continue
-    def __init__(self):
+    def __init__(self, port):
         """ChatServer Constructor"""
+        self._port = port
         self._chat_groups = {}
         self._connected_clients = {}
+        self._socket = 0
 
-    def listen():
+    def start_server(self):
+        """Initializes the server socket"""
+        ip = socket.gethostbyname(socket.getfqdn())
+        print("Starting server on port {0}".format(self._port))
+        print("Please ensure port forwarding for {0} on port {1}".format(ip, self._port))
+
+        if re.match("127.0.*", ip):
+            print("If this number is 127.0.0.1 or similar, comment out")
+            print("{0}\t{1}".format(ip, socket.getfqdn()))
+            print("in /etc/hosts")
+
+        try:
+            self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self._socket.bind((ip, self._port))
+        except:
+            print("Error initializing socket.")
+            
+            
+    def _listen(self):
         """Listen for a client"""
+        print("listening!")
 
-    def worker():
+    def _worker(self):
         """Handle a client"""
+        print("working!")
+
+    def __del__(self):
+        try:
+            self._socket.shutdown()
+            self._socket.close()
+        except:
+            print("Error closing socket. May not have been initialized")
+        finally:
+            print("Server exitting.")
 
 class Client:
     """Representation of a client"""
